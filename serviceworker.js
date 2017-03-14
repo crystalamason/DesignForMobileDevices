36963b84c0a8b005e95f517b4dfa122fd6359959
MASON Crystal A <m011220c@student.staffs.ac.uk>
2017-03-14 16:26:45 +0000

    service worker cache

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,7 @@
 var CACHE_NAME = 'gih-cache';
 var CACHED_URLS = [
   'offline.html',
-  'mystyles.css',
+  'mystyles.css’,
   'dino.png'
 ];
 
@@ -13,11 +13,16 @@ self.addEventListener('install', function(event) {
   );
 });
 
-
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
-      return caches.match('offline.html');
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('offline.html');
+        }
+      });
     })
   );
 });
